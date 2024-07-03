3501edd56e541c064c81400cd25cbdb6cf35500e
pedroenriqueps <pedroeps.18@gmail.com>
2024-07-03 18:33:16 -0300

    feat: (frontend) create forms pages authentication and login

---
+++ carteira-digital/src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import ModalLogin from "@/components/modal-forms/login-account/modal-login";
+
+export default function RenderLogin() {
+  return (
+    <>
+      <ModalLogin />
+    </>
+  );
+}
---
+++ carteira-digital/src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import ModalRegister from "@/components/modal-forms/register-account/modal-register";
+
+export default function RenderRegister() {
+  return (
+    <>
+      <ModalRegister />
+    </>
+  );
+}
