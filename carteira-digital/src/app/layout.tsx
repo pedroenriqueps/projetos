e5dab17dd024316d520df8fac87393e3740b0817
pedroenriqueps <pedroeps.18@gmail.com>
2024-07-03 18:39:41 -0300

    chore: extraneous additional codes

--- carteira-digital/src/app/layout.tsx
+++ carteira-digital/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
-
+import { AuthProvider } from "@/context/render-forms/render-forms";
 const inter = Inter({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
@@ -23,7 +23,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${inter.className} bg-slate-800 text-slate-100`}>
           <ToastContainer />
-          {children}
+          <AuthProvider>{children}</AuthProvider>
         </body>
       </html>
     </>
