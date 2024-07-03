f42d82665875829b751988190f167f996254d7c1
pedroenriqueps <pedroeps.18@gmail.com>
2024-07-03 18:37:06 -0300

    refactor: (frontend) developing page message

---
+++ carteira-digital/src/app/recharts-password/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function RechartsPassword() {
+  return (
+    <>
+      <h2>página de recuperação de senha, em desenvolvimento</h2>
+      <Link href="/">Página inicial</Link>
+      <Link href="/auth/register">Página de cadastro</Link>
+    </>
+  );
+}
